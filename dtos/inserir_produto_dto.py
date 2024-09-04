7914bb6a311ff0ed9f5563a3bdfa14fda75b5d37
Igor <igor.almeidadasilva13@gmail.com>
2024-09-04 16:38:51 -0300

    alteracao e obtencao de um produto

--- dtos/inserir_produto_dto.py
+++ dtos/inserir_produto_dto.py
@@ -1,10 +1,9 @@
 from pydantic import BaseModel, field_validator
-from datetime import date, datetime, timedelta
 
 from util.validators import *
 
 
-class NovoProdutoDTO(BaseModel):
+class InserirProdutoDTO(BaseModel):
     nome: str
     preco: float
     descricao: str
@@ -12,30 +11,25 @@ class NovoProdutoDTO(BaseModel):
     
     @field_validator("nome")
     def validar_nome(cls, v):
-        msg = is_project_name(v, "Nome")
-        if msg:
-            raise ValueError(msg)
+        msg = is_size_between(v, "Nome", 2, 128)
+        if msg: raise ValueError(msg)
         return v
 
     @field_validator("preco")
     def validar_preco(cls, v):
         msg = is_in_range(v, "Preço", 0.0, 100000.0)
-        if msg:
-            raise ValueError(msg)
+        if msg: raise ValueError(msg)
         return v
 
     @field_validator("descricao")
     def validar_descricao(cls, v):
         msg = is_not_empty(v, "Descrição")
-        if not msg:
-            msg = is_min_size(v, "Descrição", 16)
-        if msg:
-            raise ValueError(msg)
+        msg = msg or is_size_between(v, "Descrição", 16, 1024)
+        if msg: raise ValueError(msg)
         return v
 
     @field_validator("estoque")
     def validar_estoque(cls, v):
-        msg = is_in_range(v, "Endereço", 0, 1000)
-        if msg:
-            raise ValueError(msg)
+        msg = is_in_range(v, "Estoque", 0, 1000)
+        if msg: raise ValueError(msg)
         return v
--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,17 +1,44 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path, Query, Response
+from fastapi.responses import JSONResponse
 
-from dtos.novo_produto_dto import NovoProdutoDTO
+from dtos.alterar_produto_dto import AlterarProdutoDTO
+from dtos.id_produto_dto import IdProdutoDTO
+from dtos.inserir_produto_dto import InserirProdutoDTO
+from dtos.problem_details_dto import ProblemDetailsDto
+from models.produto_model import Produto
 from repositories.produto_repo import ProdutoRepo
 
 
 router = APIRouter(prefix="/manager")
 
 
-@router.get("obter_produtos")
+@router.get("/obter_produtos")
 async def obter_produtos():
     produtos = ProdutoRepo.obter_todos()
     return produtos
 
-@router.post("inserir_produto")
-async def inserir_produto(produto: NovoProdutoDTO):
-    pass
+@router.post("/inserir_produto", status_code=201)
+async def inserir_produto(inputDto: InserirProdutoDTO) -> Produto:
+    novo_produto = Produto(None, inputDto.nome, inputDto.preco, inputDto.descricao, inputDto.estoque)
+    novo_produto = ProdutoRepo.inserir(novo_produto)
+    return novo_produto
+
+@router.post("/excluir_prouto", status_code=204)
+async def excluir_produto(inputDto: IdProdutoDTO) -> bool:
+    if ProdutoRepo.excluir(inputDto.id_produto): return None
+    pb = ProblemDetailsDto("int", f"O produto com id {inputDto.id_produto} não foi encontrado","value_not_found", ["body", "id_produto"])
+    return JSONResponse(pb.to_dict(), status_code=404)
+
+@router.get("/obter_produto/{id_produto}")
+async def obter_produto(id_produto: int = Path(..., title="Id do Produto", ge=1)) -> bool:
+    produto = ProdutoRepo.obter_um(id_produto)
+    if produto: return produto
+    pb = ProblemDetailsDto("int", f"O produto com id {id_produto} não foi encontrado","value_not_found", ["body", "id_produto"])
+    return JSONResponse(pb.to_dict(), status_code=404)
+
+@router.post("/alterar_prouto", status_code=204)
+async def alterar_produto(inputDto: AlterarProdutoDTO) -> bool:
+    produto = Produto(inputDto.id, inputDto.nome, inputDto.preco, inputDto.descricao, inputDto.estoque)
+    if ProdutoRepo.alterar(produto): return None
+    pb = ProblemDetailsDto("int", f"O produto com id {inputDto.id_produto} não foi encontrado","value_not_found", ["body", "id_produto"])
+    return JSONResponse(pb.to_dict(), status_code=404)
